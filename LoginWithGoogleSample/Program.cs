4660533e05763d55524cee7f3ac4e46b54d9a310
ToPyro <ductu007hn@gmail.com>
2024-06-17 20:28:08 +0700

    Add GoogleAuth
    App not secured - bug#1

--- LoginWithGoogleSample/Program.cs
+++ LoginWithGoogleSample/Program.cs
@@ -1,9 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration.GetValue<string>("Authentication:Google:client_id");
+    options.ClientSecret = builder.Configuration.GetValue<string>("Authentication:Google:client_secret");
+    options.CallbackPath = "/sigin-google";
+    options.SaveTokens = true;
+});
+
+builder.Services.AddSession(options =>
+{
+    /*options.IdleTimeout = TimeSpan.FromMinutes(1);*/ //Default 20 mins
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,14 +43,24 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Content-Security-Policy", "script-src 'self' https://accounts.google.com/gsi/client;" +
+        " frame-src 'self' https://accounts.google.com/gsi/;" +
+        " connect-src 'self' https://accounts.google.com/gsi/");
+    context.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    //context.Response.Headers.Add("Cross-Origin-Opener-Policy-Report-Only", "same-origin-allow-popups");
+    await next();
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseSession();
 app.MapRazorPages();
+app.MapControllers();   
 
 app.Run();
